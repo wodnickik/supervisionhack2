7974f90d37c16c5218383b2625909be9eac30cf1
Krzysztof Wodnicki <woodoo650@gmail.com>
2023-05-21 08:09:41 +0200

    dokumentacja

--- scraper/context_generator.py
+++ scraper/context_generator.py
@@ -16,7 +16,23 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import StaleElementReferenceException
 
 
-def generate_context(keywords, driver=None, cookies_out=None):
+# ta funkcja ma tylko bardzo podstawową funkcjonalność
+# żeby pokazać poc naszego pomysłu na generowanie kontekstu
+def generate_context(
+keywords: list(str),
+driver: webdriver = None,
+cookies_out: str = None):
+    """
+    Generuje kontekst którego można użyć do scrapowania reklam,
+    najlepiej podać przyjanmniej jedno z dwóch driver/cookies_out bo inaczej kontekst się gdzieś zgubi
+    :param keywords: lista fraz kluczowych do generowania kontekstu
+    :param driver: opcjonalny parametr w którym można przekazać driver żeby nabijać kontekst bezpośrednio w sesji
+    :param cookies_out: opcjonalny parametr z lokalizcją do zapisu ciasteczek
+    :return: brak
+    """
+
+    # jeśli nie otrzymujemy na wejściu drivera
+    # to tworzymy nowy i pamiętamy, że trzeba go na koniec zamknąć
     if driver is None:
         flag = True
 
@@ -31,13 +47,20 @@ def generate_context(keywords, driver=None, cookies_out=None):
         driver = webdriver.Chrome(options=options)
 
     # TODO zaakceptować używanie plików cookie w google
+
+    # dla każdego słowa kluczowego wyszukujemy je w googlu
     for keyword in keywords:
         driver.get('https://www.google.com/search?q=' + keyword.replace(' ', '+'))
+        # dalej moglibyśmy na przykąłd klikać losowe linki z wyniku
+        # akceptować cookies na stronach na które trafimy
+        # i w ten sposób tworzyć kontekst
         time.sleep(2)
 
+    # zapisujemy plik z kontekstem
     if cookies_out is not None:
         with open(cookies_out, "w") as file:
             json.dump(driver.get_cookies(), file)
 
+    # jeśli jest taka potrzeba zamykamy driver
     if flag:
         driver.quit()
--- scraper/google_ads_downloader.py
+++ scraper/google_ads_downloader.py
@@ -16,11 +16,34 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import StaleElementReferenceException
 
 
-def hash_encode(s):
+def hash_encode(s: str):
+    """
+    Wygeneruj hash
+    :param s: napis
+    :return: hash
+    """
     return hashlib.md5(s.encode()).hexdigest()
 
 
-def download_ads(url: str, output_destination='out', cookies_location='cookies', verbose=0):
+# tej funkcji zdarza się rzucić wyjątek
+# prawdopodobnie lepsze waity rozwiążą problem
+# odpalenie jej drugi raz prawie zawsze to naprawia
+def download_ads(
+    url: str,
+    output_destination: str = 'out',
+    cookies_location: str = 'cookies',
+    verbose: bool = 0
+):
+    """
+    Scrapeuje google adsy z zadanego urla
+    :param url: url strony do scrapowania
+    :param output_destination: folder w którym zostaną zapisane jsony i pngi
+    :param cookies_location: folder z kontekstem
+    :param verbose: czy printować
+    :return: create json file with metadata, cookies context and ad images
+    """
+
+    # opcje potrzebne do prawidłowego funkcjonowania drivera
     options = Options()
     options.add_argument("--start-maximized")
     options.add_argument("--disable-infobars")
@@ -30,28 +53,38 @@ def download_ads(url: str, output_destination='out', cookies_location='cookies',
         "prefs", {"profile.default_content_setting_values.notifications": 1}
     )
 
+    # pusta lista na metadane reklam
     json_ads = []
 
+    # tworzenie drivera
     with webdriver.Chrome(options=options) as driver:
 
-
+        # wczytywanie cookies potrzebnych do obejścia
+        # wyskakującego okienka "zaakceptuj pliki cookies"
+        # trzeba mieć wcześniej plik cookies z tej witryny
+        # na niektórych witrynach nie działa, wtedy trzeba specjalnie dalej kombinować (na przykład wp)
+        # jeśli nie uda się obejść "zaakceptuj pliki cookies" to po prostu nie pobierze reklam ze strony
+        # ale się nie wywali
         driver.get(url)
         if verbose: print(driver.current_url)
         domain = urlparse(driver.current_url).netloc
         cookies = json.loads(Path(path.join(cookies_location, f'{domain}_cookies.json')).read_text())
 
-
-        # analogicznie do wczyatania cookies do obejścia klikania zgody na cookies
-        # można wczytać dowolny kontekst z plików cookies
+        # można ładować ciasteczka z kilku plików
+        # w ten sposób można dodać ładowanie dodatkowego kontekstu
         for cookie in cookies:
             if 'sameSite' in cookie:
                 cookie['sameSite'] = 'Strict'
             driver.add_cookie(cookie)
-
+        # ładujemy stronę jeszcze raz po załądowaniu cookies
+        # żeby pozbyć się popupu "zaakceptuj cookies"
+        # to bardzo sprytne, prosimy docenić pomysłowość workarounda :)
         driver.get(url)
 
+        # wyrażenie charakterystyczne dla google ads
         pattern = re.compile("^google_ads_iframe.*")
 
+        # wybieranie elementów z reklamami
         elements = []
         for i in range(2):
             time.sleep(2)
@@ -59,23 +92,26 @@ def download_ads(url: str, output_destination='out', cookies_location='cookies',
             driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
         elements = [_ for _ in elements if pattern.match(_.get_attribute('id'))]
 
+        # filter dla regexa
         tmp = dict()
         for element in elements:
             tmp[element.get_attribute('id')] = element
         elements = tmp
         if verbose: print(len(elements))
 
+        # po przefiltrowaniu iterujemy się po każdym elemencie
         for key in elements:
             element = elements[key]
             ActionChains(driver).move_to_element(element).perform()
-            # driver.execute_script("arguments[0].scrollIntoView();", element)
             time.sleep(1)
 
+            # robimy zdjęcie reklamy
             WebDriverWait(driver, 10, ignored_exceptions=StaleElementReferenceException).until(
                 EC.presence_of_element_located((By.ID, key)))
             hash_value = hash_encode(f'{driver.current_url}_{datetime.now()}')
             element.screenshot(path.join(output_destination, f'{hash_value}.png'))
 
+            # pobieramy docelowy url
             ActionChains(driver).click(element).perform()
             parent = driver.window_handles[0]
             child = driver.window_handles[-1]
@@ -84,13 +120,16 @@ def download_ads(url: str, output_destination='out', cookies_location='cookies',
             driver.close()
             driver.switch_to.window(parent)
 
+            # zbiermay metadane
             json_dict = {"id": hash_value,
                          "img_name": f'{hash_value}.png',
                          "link": ad_url}
             json_ads.append(json_dict)
 
+        # zapisujemy cookies
         with open(path.join(output_destination, "cookies.json"), "w") as file:
             json.dump(driver.get_cookies(), file)
 
+    # zapisujemy metadane
     with open(path.join(output_destination, "metadata.json"), "w") as file:
         json.dump(json_ads, file)
